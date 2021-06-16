5b4e74ed21a25248f8c6495cfdf8273815264665
Maksim Seregin <maks.seregin2011@gmail.com>
2021-06-16 11:19:37 +0300

    no message

---
+++ android/app/src/main/kotlin/com/example/nevitemsezon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nevitemsezon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/UI/MeinItem.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MeinItem extends StatefulWidget {
+  const MeinItem({Key? key}) : super(key: key);
+
+  @override
+  _MeinItemState createState() => _MeinItemState();
+}
+
+class _MeinItemState extends State<MeinItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
